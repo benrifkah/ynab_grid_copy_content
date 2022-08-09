6a42dfe8623f7bab007fab5b950d85448b06c3ac
BenRifkah Bergsten-Buret <94815+benrifkah@users.noreply.github.com>
2022-08-09 10:02:30 -0700

    initial commit

--- code.js
+++ code.js
@@ -1,7 +1,24 @@
-
 let wanted_cells = [
-  'date', 'payeeName', 'outflow', 'inflow'
+  'accountName', 'date', 'payeeName', 'outflow', 'inflow', 'memo'
 ];
+
+function scaleBuilder (collector,item,idx) {
+  console.log("item:", item, 'idx:', idx, "collector:", collector);
+  collector[item] = idx;
+  return collector
+}
+
+let weights = wanted_cells.reduce(scaleBuilder,{});
+console.log("weights:", weights);
+
+function fieldSorter (a, b) {
+  let a_field = Array.from(a.classList).filter(cls => cls.match(/ynab-grid-cell-/))[0].match(/-(\w+)$/)[1];
+  let b_field = Array.from(b.classList).filter(cls => cls.match(/ynab-grid-cell-/))[0].match(/-(\w+)$/)[1];
+  let a_weight = weights[a_field];
+  let b_weight = weights[b_field];
+  console.log("a_field:", a_field, "a_weight:", a_weight,"b_field:", b_field,"b_weight:", b_weight);
+  return weights[a_field] - weights [b_field];
+}
 
 function getCellText(cell) {
   let span = cell.querySelector('span');
@@ -13,7 +30,7 @@ function getCellText(cell) {
   }
   console.log('  text:', text);
   return text;
-}
+a}
 
 function getRowSummary(row, idx) {
   console.log('row:', row);
@@ -21,18 +38,20 @@ function getRowSummary(row, idx) {
     c => ':scope > .ynab-grid-cell-' + c
   ).join(",");
   let cells = row.querySelectorAll(wanted_cells_selector);
+  let sorted_cells = Array.from(cells).sort(fieldSorter);
+  console.log("sorted_cells:", sorted_cells); /*FIXME-ben*/
   let header = (idx + 1).toString() + '. ';
-  let fields = Array.from(cells).map(getCellText).join(", ");
+  let fields = sorted_cells.map(getCellText).join(", ");
   return header + fields;
 }
 
 function getGridSummary() {
   let rows = document.querySelectorAll('[data-row-id]');
-  header = wanted_cells.join(", ");
+  header = "#. " + wanted_cells.join(", ");
   let lines = Array.from(rows).map(getRowSummary);
   lines.unshift(header);
   let summary = lines.join("\n");
-  console.log('summary:', summary);
+  console.log("summary:\n", summary);
   navigator.clipboard.writeText(summary).then(function () {
     alert("copied to clipboard - summary:\n" + summary);
   });
